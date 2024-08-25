12c7eb06e022a981a03e2b1b1fd8f7c0cd060ff8
jkeus <jkeus619@gmail.com>
2024-08-25 01:34:32 -0700

    fixing card issue with resizing

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -3,8 +3,8 @@ import styles from './ProjectCardStyles.module.css'
 
 function ProjectCard({ src, link, h3, p }) {
   return (
-    <div className={styles.projectCard}>
-      <a href={link} target="_blank" className={styles.projectCardContent}>
+    <div>
+      <a href={link} target="_blank" >
         <img className="hover" src={src} alt={`${h3} logo`} />
         <h3>{h3}</h3>
         <p>{p}</p>
